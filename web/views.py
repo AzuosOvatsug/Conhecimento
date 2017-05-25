b621a2f87214f570df0657fdaf0d236abf3a1706
GustavoSouza <gustavo.ifto@gmail.com>
2017-05-25 18:16:15 -0300

    Implatação de alguns filtros e metodos para deixar as coisas mais simples

--- web/views.py
+++ web/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'web/post_list.html', {})
+    Post.objects.filter(publicado_data__lte=timezone.now()).order_by('publicado_data')
+    return render(request, 'web/post_list.html', {'posts': posts})
