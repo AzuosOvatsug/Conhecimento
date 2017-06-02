f52b6052db03eded3a4c9bc5b3bcd8e72f38b9f7
GustavoSouza <gustavo.ifto@gmail.com>
2017-06-02 14:34:11 -0300

    Tentando usar QuerySet

--- web/models.py
+++ web/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    autor = models.ForeignKey('Autor')
+    autor = models.ForeignKey('auth.User')
     titulo = models.CharField(max_length=200)
     texto = models.TextField()
     data_criacao = models.DateTimeField(default=timezone.now)
--- web/views.py
+++ web/views.py
@@ -3,5 +3,5 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    #posts = Post.objects.filter(publicado_data__lte=timezone.now()).order_by('publicado_data')
-    return render(request, 'web/post_list.html', {})
+    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'web/post_list.html', {'posts': posts})
